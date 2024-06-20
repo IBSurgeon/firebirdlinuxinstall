43c25081fff8abbdb1b5021facdcc0465969c043
Alexey Kovyazin <support@ibknowledgebase.com>
2024-06-20 11:33:25 +0300

    Debian 11 and 12 for vanilla Firebird

---
+++ debian12/fb_vanilla_deb12-50.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Please contact IBSurgeon with any question regarding this script: support@ib-aid.com
+# This script is provided AS IS, without any warranty. 
+# This script is licensed under IDPL https://firebirdsql.org/en/initial-developer-s-public-license-version-1-0/
+
+FB_VER=5.0
+FB_URL="https://github.com/FirebirdSQL/firebird/releases/download/v5.0.0/Firebird-5.0.0.1306-0-linux-x64.tar.gz"
+TMP_DIR=$(mktemp -d)
+OLD_DIR=$(pwd -P)
+
+download_file(){
+    url=$1
+    tmp=$2
+    name=$3
+    fname=$(basename -- "$url")
+
+    echo "Downloading $name..."
+    curl --location $url --output $tmp/$fname --progress-bar
+
+    case $? in
+      0)  echo "OK";;	  
+      23) echo "Write error"
+          exit 0;;
+      67) echo "Wrong login / password"
+              exit 0;;
+      78) echo "File $url does not exist on server"
+          exit 0;;
+    esac
+}
+
+echo "vm.max_map_count = 256000" >> /etc/sysctl.conf
+sysctl -p
+
+apt update 
+apt install --no-install-recommends -y net-tools libtommath1 libicu72 wget unzip gettext libncurses5 curl tar tzdata locales sudo xz-utils file apt-transport-https gpg libatomic1
+
+ln -s libtommath.so.1 /usr/lib/x86_64-linux-gnu/libtommath.so.0 
+locale-gen "en_US.UTF-8"
+
+download_file $FB_URL $TMP_DIR "FB installer"
+
+echo Extracting FB installer ==================================================
+
+mkdir $TMP_DIR/fb
+tar xvf $TMP_DIR/*.gz -C $TMP_DIR/fb --strip-components=1 > /dev/null
+cd $TMP_DIR/fb
+
+echo Running FB installer =====================================================
+
+yes 'masterkey' | ./install.sh
+#./install.sh -silent
+cd $OLD_DIR
+
+# cleanup
+if [ -d $TMP_DIR ]; then rm -rf $TMP_DIR; fi
